0361504031df7e7e6579546c29c9f966fe35662e
tojonatolotra <tojonatolotraranaivozanany@outlook.fr>
2025-07-11 08:58:41 +0000

    🔨 UP

--- MyApp.Application/Interfaces/InputPort/IWithdrawMoneyUseCase.cs
+++
@@ -1,9 +0,0 @@
-﻿using MyApp.Application.DTOs;
-
-namespace MyApp.Application.Interfaces.InputPort
-{
-    public interface IWithdrawMoneyUseCase
-    {
-        Task<WithdrawMoneyResponse> Handle(WithdrawMoneyRequest request);
-    }
-}
